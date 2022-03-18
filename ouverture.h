e3a24c416bf468b501418e47a59a71969fff2528
D0ko <98818143+D0ko@users.noreply.github.com>
2022-03-18 11:37:05 +0100

    Create ouverture.h

---
+++ ouverture.h
@@ -0,0 +1,11 @@
+/* ouverture.h */
+
+#ifndef __ouverture__
+#define __ouverture__
+
+    #include <stdio.h>
+
+    int taille_fichier(FILE* fichier);
+    char* lecture_fichier(char* nom);
+
+#endif
