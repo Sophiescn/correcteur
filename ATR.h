8a82efdcfd7e4d4b37e2744b19e7df1f85cb5ce9
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-18 10:34:20 +0100

    Update ATR.h

--- ATR.h
+++ ATR.h
@@ -1,20 +1,22 @@
-/* Auteur : Quentin Benesby Sophie Cousson
-* Creation : 05-03-2022
-* Modification :  */
-
-/* ATR.h */
-
 #ifndef __ATR__
 #define __ATR__
-
-    include "Liste.h"
 
     typedef struct noeud {
         char c;
         struct noeud* fg;
         struct noeud* fd;
         struct noeud* fils;
-    } Noeud, * ATR;
+    } Noeud, *ATR;
 
+    ATR creer_ATR_vide();
+    Noeud* alloue_arbre(char lettre);
+    void liberer_ATR(ATR* A);
+    void affiche_aux(ATR A, char buffer[], int position);
+    void afficher_ATR(ATR A);
+    int recherche(ATR A, char* mot);
+    void ajoute_branche(ATR* A, char* mot);
+    int inserer_dans_ATR(ATR* A, char* mot);
+    void suppression_aux(ATR A, ATR* tmp, char* mot, char buffer[], int position);
+    void supprimer_dans_ATR(ATR* A, char* mot);
 
 #endif
