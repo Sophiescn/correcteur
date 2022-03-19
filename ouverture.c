93eda838cf2b46dc3d51cd5caeaccc83ee5eeed3
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-19 10:14:16 +0100

    Update ouverture.c

--- ouverture.c
+++ ouverture.c
@@ -3,6 +3,28 @@
 #include <string.h>
 #include <ctype.h>
 
+int fichier_valide(char* fichier) {
+    FILE* f;
+    f = fopen(fichier, "r");
+    if (f){
+        fclose(f);
+        return 1;
+    }
+    return 0;
+}
+
+
+int arguments_valides (int argc, char* argv[]) {
+    if (argc != 3){
+        return 0;
+    }
+    if (!fichier_valide(argv[1]) || !fichier_valide(argv[2])) {
+        return 0;
+    }
+    return 1;
+}
+
+
 int taille_fichier(FILE* fichier) {
     int taille;
     fseek(fichier, 0, SEEK_END);
