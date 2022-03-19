863bac5f9e45817717fa61aee344042b4c958047
D0ko <98818143+D0ko@users.noreply.github.com>
2022-03-20 00:07:59 +0100

    Create correcteur.h

---
+++ correcteur.h
@@ -0,0 +1,11 @@
+/* correcteur.h */
+
+#ifndef __correcteur__
+#define __correcteur__
+
+    #include "Liste.h"
+    #include "ATR.h"
+
+    Cellule* correction(Cellule* liste, Noeud* dico);
+
+#endif
