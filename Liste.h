2d68fbe11590d195d2097551fd737cfd76a816c1
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-19 09:28:21 +0100

    Update Liste.h

--- Liste.h
+++ Liste.h
@@ -1,18 +1,20 @@
+/* Liste.h */
+
 /* Auteur : Quentin Benesby Sophie Cousson
 * Creation : 05-03-2022
 * Modification :  */
 
-/* Liste.h */
-
 #ifndef __Liste__
 #define __Liste__
+
     typedef struct cellule {
         char* mot;
         struct cellule* next_cell;
-    }Cellule, * Liste;
+    } Cellule, * Liste;
 
     Cellule * allouer_Cellule(char * mot);
-    int inserer_en_tete(Liste * L, char * mot);
-
+    int inserer_en_tete(Liste* L, char * mot);
+    void liberer_liste(Liste* L);
+    void afficher_liste(Liste L);
 
 #endif
