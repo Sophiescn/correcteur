8b015e82e197bc12af068f83db5b94533ef9ca65
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-19 10:13:29 +0100

    Update ouverture.h

--- ouverture.h
+++ ouverture.h
@@ -5,6 +5,8 @@
 
     #include <stdio.h>
 
+    int fichier_valide(char* fichier);
+    int arguments_valides (int argc, char* argv[]);
     int taille_fichier(FILE* fichier);
     char* lecture_fichier(char* nom);
 
