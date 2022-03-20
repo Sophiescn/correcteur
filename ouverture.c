f330a9a6d415f2e48f5710f6acd5ccae7b7d923d
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-20 11:42:16 +0100

    Update ouverture.c

--- ouverture.c
+++ ouverture.c
@@ -13,6 +13,15 @@ int fichier_valide(char* fichier) {
     return 0;
 }
 
+int est_dictionnaire(char* chaine) {
+    int i;
+    for (i = 0; i < strlen(chaine); i++){
+        if (chaine[i] == '.' && chaine[i + 1] == 'd') {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int arguments_valides (int argc, char* argv[]) {
     if (argc != 3){
@@ -21,9 +30,27 @@ int arguments_valides (int argc, char* argv[]) {
     if (!fichier_valide(argv[1]) || !fichier_valide(argv[2])) {
         return 0;
     }
-    return 1;
+    
+    if (est_dictionnaire(argv[1])) {
+        if (est_dictionnaire(argv[2])) {
+            return 0;
+        }
+        return 1;
+    }
+    if (est_dictionnaire(argv[2])) {
+        return 2;
+    }
+    return 0;
 }
 
+void ordonne_fichiers(int res, char* source_1, char* source_2) {
+    char* temp;
+    if (res == 1) {
+        temp = source_1;
+        source_1 = source_2;
+        source_2 = temp;
+    }
+}
 
 int taille_fichier(FILE* fichier) {
     int taille;
