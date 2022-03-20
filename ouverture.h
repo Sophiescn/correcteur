80adffd044aeae7f61b13e883d653d2400ad80bb
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-03-20 11:43:11 +0100

    Update ouverture.h

--- ouverture.h
+++ ouverture.h
@@ -6,7 +6,9 @@
     #include <stdio.h>
 
     int fichier_valide(char* fichier);
+    int est_dictionnaire(char* chaine);
     int arguments_valides (int argc, char* argv[]);
+    void ordonne_fichiers(int res, char* source_1, char* source_2);
     int taille_fichier(FILE* fichier);
     char* lecture_fichier(char* nom);
 
