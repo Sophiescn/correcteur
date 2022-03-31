8a7c8d042719029cfa560e6f318913ab79256c11
D0ko <98818143+D0ko@users.noreply.github.com>
2022-03-31 13:29:58 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+/* Auteur : Quentin Benesby Sophie Cousson
+* Creation : 05-03-2022
+* Modification :  20-03-2022*/
+
 #include "ATR.h"
 #include "ouverture.h"
 #include "Liste.h"
@@ -5,32 +9,47 @@
 #include <stdio.h>
 
 
-int main(void) {
+int main(int argc, char* argv[]) {
     ATR a;
+    Liste l;
+    Liste erreurs;
     char* dico, *liste;
     char* source_1, *source_2;
     int res;
-  
+
     source_1 = argv[1];
     source_2 = argv[2];
+
+
     if (!(res = arguments_valides(argc, argv))) {
         return 0;
     }
 
-    ordonne_fichiers(res, source_1, source_2);
+    if (res == 1) {
+        dico = lecture_fichier(source_1);
+        liste = lecture_fichier(source_2);
+    } else {
+        dico = lecture_fichier(source_2);
+        liste = lecture_fichier(source_1);
+    }
 
-    dico = lecture_fichier("dico_1.dico");
-    liste = lecture_fichier("a_corriger_0.txt");
+
 
     a = initialiser_ATR(dico);
     l = initialiser_liste(liste);
 
     erreurs = correction(l, a);
 
-    printf("Mot(s) mal orthographié(s) :\n");
+    printf("Mot(s) mal orthographie(s) :\n");
     if (erreurs) {
         afficher_liste(erreurs);
+    } else {
+        printf("aucun\n");
     }
-    printf("aucun\n");
+
+    liberer_liste(&l);
+    liberer_liste(&erreurs);
+    liberer_ATR(&a);
+
     return 0;
 }
