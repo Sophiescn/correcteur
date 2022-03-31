73e38161119ad556e7252289cf3bcf4e777e0006
D0ko <98818143+D0ko@users.noreply.github.com>
2022-03-31 14:26:54 +0200

    Create correcteur_1.c

---
+++ correcteur_1.c
@@ -0,0 +1,56 @@
+/* Auteur : Quentin Benesby Sophie Cousson
+* Creation : 21-03-2022
+* Modification :  31-03-2022 */
+
+#include "ATR.h"
+#include "ouverture.h"
+#include "Liste.h"
+#include "correcteur.h"
+#include "levenshtein.h"
+#include <stdio.h>
+
+
+int main(int argc, char* argv[]) {
+    ATR a;
+    Liste l;
+    Liste erreurs;
+    char* dico, *liste;
+    char* source_1, *source_2;
+    int res;
+
+    source_1 = argv[1];
+    source_2 = argv[2];
+
+
+    if (!(res = arguments_valides(argc, argv))) {
+        return 0;
+    }
+
+    if (res == 1) {
+        dico = lecture_fichier(source_1);
+        liste = lecture_fichier(source_2);
+    } else {
+        dico = lecture_fichier(source_2);
+        liste = lecture_fichier(source_1);
+    }
+
+
+
+    a = initialiser_ATR(dico);
+    l = initialiser_liste(liste);
+
+    erreurs = correction(l, a);
+
+    printf("Mot(s) mal orthographie(s) :\n");
+    if (erreurs) {
+        afficher_liste(erreurs);
+    } else {
+        printf("aucun\n");
+    }
+
+    liberer_liste(&l);
+    liberer_liste(&erreurs);
+    liberer_ATR(&a);
+
+    return 0;
+}
