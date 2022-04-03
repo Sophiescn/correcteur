9053cc2bb6f8f2703409573e8779ca88f6f9be81
D0ko <98818143+D0ko@users.noreply.github.com>
2022-04-03 15:23:24 +0200

    Update correcteur_1.c

--- correcteur_1.c
+++ correcteur_1.c
@@ -1,6 +1,6 @@
 /* Auteur : Quentin Benesby Sophie Cousson
 * Creation : 21-03-2022
-* Modification :  31-03-2022 */
+* Modification :  03-04-2022 */
 
 #include "ATR.h"
 #include "ouverture.h"
@@ -12,15 +12,18 @@
 
 int main(int argc, char* argv[]) {
     ATR a;
+    Liste dico_liste;
     Liste l;
     Liste erreurs;
+    Liste proposition;
+    Cellule* courant_erreurs;
+    Cellule* courant_proposition;
     char* dico, *liste;
     char* source_1, *source_2;
     int res;
 
     source_1 = argv[1];
     source_2 = argv[2];
-
 
     if (!(res = arguments_valides(argc, argv))) {
         return 0;
@@ -38,14 +41,36 @@ int main(int argc, char* argv[]) {
 
     a = initialiser_ATR(dico);
     l = initialiser_liste(liste);
+    dico_liste = initialiser_liste(dico);
 
     erreurs = correction(l, a);
 
-    printf("Mot(s) mal orthographie(s) :\n");
-    if (erreurs) {
-        afficher_liste(erreurs);
-    } else {
+    if (!erreurs) {
         printf("aucun\n");
+        return 0;
+    }
+
+    courant_erreurs = erreurs;
+
+    while (courant_erreurs) {
+
+        proposition = force_brute(courant_erreurs->mot, dico_liste);
+        courant_proposition = proposition;
+
+    
+        printf("Mot mal orthographie : %s\n", courant_erreurs->mot);
+        printf("Proposition(s) : ");
+
+
+        while (courant_proposition) {
+            printf("%s ", courant_proposition->mot);
+            courant_proposition = courant_proposition->next_cell;
+        }
+        printf("\n");
+        liberer_liste(&proposition);
+
+
+        courant_erreurs = courant_erreurs->next_cell;
     }
 
     liberer_liste(&l);
