b0f6d71b5fd573ee48baf4c5df5934a16a56b749
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-04-14 13:26:50 +0200

    Update ArbreBK.c

--- ArbreBK.c
+++ ArbreBK.c
@@ -1,1 +1,24 @@
 #include "ArbreBK.h"
+#include <stdlib.h>
+
+NoeudBK* alloue_arbreBK(char* mot, int valeur) {
+    NoeudBK* noeud;
+    noeud = (NoeudBK*) malloc(sizeof(NoeudBK));
+    if (noeud) {
+        noeud->mot = mot;
+        noeud->valeur = valeur;
+        noeud->frereD = NULL;
+        noeud->filsG = NULL;
+    }
+    return noeud;
+}
+
+int inserer_dans_ArbreBK(ArbreBK * A, char * mot) {
+    if (!A) {
+        *A = alloue_arbreBK(mot, 0);
+        if (A) {
+            return 1;
+        }
+        return 0;
+    }
+}
