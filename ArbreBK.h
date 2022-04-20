37423a78b0d2ccba43d218dac41565839a603812
Sophiescn <96613035+Sophiescn@users.noreply.github.com>
2022-04-20 19:09:38 +0200

    Update ArbreBK.h

--- ArbreBK.h
+++ ArbreBK.h
@@ -1,13 +1,24 @@
-#ifndef __BK__
-#define __Bk__
+/* ArbreBK.h */
 
-    typedef struct noeudBK{
+#ifndef __BK__
+#define __BK__
+
+    typedef struct noeudBK {
         char* mot;
         int valeur;
-        struct noeudBK* filsG; /* ?? */
-        struct noeudBK* frereD;
+        struct noeudBK* fils;
+        struct noeudBK* frere;
     } NoeudBK, * ArbreBK;
 
+    NoeudBK* alloue_arbreBK(char* mot, int leven);
+    int inserer_dans_ArbreBK_suite(ArbreBK* A, char* mot, int leven);
     int inserer_dans_ArbreBK(ArbreBK * A, char * mot);
+    int rechercher_dans_ArbreBK_aux(ArbreBK A, char* mot, int leven);
+    int rechercher_dans_ArbreBK(ArbreBK A, char* mot);
+    void libere_noeudBK(NoeudBK** A);
+    void liberer_ArbreBK(ArbreBK* A);
+    void afficher_ArbreBK_aux(ArbreBK A, int profondeur);
+    void afficher_ArbreBK(ArbreBK A);
+    ArbreBK initialiser_BK(char* chaine);
     
 #endif
