09072467c3b2e08187c51affa303f405624341d1
D0ko <98818143+D0ko@users.noreply.github.com>
2022-04-21 22:54:35 +0200

    Update correcteur.h

--- correcteur.h
+++ correcteur.h
@@ -8,10 +8,12 @@
 #define __correcteur__
 
     #include "Liste.h"
+    #include "ArbreBK.h"
     #include "ATR.h"
 
     Cellule* correction(Cellule* liste, Noeud* dico, ArbreBK dicoB);
     Cellule* force_brute(char* mot, Liste liste_dico);
+    ArbreBK correction_BK_aux(char* mot, ArbreBK a);
     Cellule* correction_BK(char* mot, ArbreBK dico);
 
 #endif
